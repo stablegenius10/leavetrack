2f668e8560d0b2b4b7ed6a95b370101d19a5d6de
stablegenius10 <115222029+stablegenius10@users.noreply.github.com>
2024-04-29 17:22:41 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb+srv://ayush:ayush@cluster0.hkkuuyw.mongodb.net/")
+  .connect("mongodb+srv://ayush:ayush@cluster0.hkkuuyw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
